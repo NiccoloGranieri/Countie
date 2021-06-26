f1333fdd4495d6628a94549b70521b13551822f9
Niccolò <granieriniccolo@gmail.com>
2021-06-26 17:15:14 +0100

    Add empty Swift.app

---
+++ Countie/CountieApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountieApp.swift
+//  Countie
+//
+//  Created by Niccolo Granieri on 26/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct CountieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
